3a5b5d75818a7fdef4a7b1f7115ede311d1f3817
pdtunes <55880640+pdtunes@users.noreply.github.com>
2021-06-05 13:31:03 +0200

    removed login and admin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
-import LoginPage from "./components/login/LoginPage";
-import TeamsPage from "./components/dashboard/teams/TeamsPage";
 import DetailsPage from "./components/dashboard/teams/DetailsPage";
 import Nav from "./components/layout/Nav";
 import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
-import Admin from "./components/dashboard/Admin";
 import TrackList from "./components/dashboard/tracks/TrackList";
 import TrackDetails from "./components/dashboard/tracks/TrackDetails";
 import About from "./components/about/About";
@@ -27,20 +24,11 @@ function App() {
               <Route exact path="/about">
                 <About />
               </Route>
-              <Route path="/login">
-                <LoginPage />
-              </Route>
-              <Route path="/admin">
-                <Admin />
-              </Route>
               <Route path="/tracks" exact>
                 <TrackList />
               </Route>
               <Route path="/tracks/details/:id">
                 <TrackDetails />
-              </Route>
-              <Route path="/dashboard/teams" exact>
-                <TeamsPage />
               </Route>
               <Route path="/teams/details/:id">
                 <DetailsPage />
